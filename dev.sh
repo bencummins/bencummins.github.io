822a68cb5ab100fd4f9a4ff7ceca196f5ee20148
Ben Cummins <bcummins@Bens-MBP-3.lan>
2019-03-04 00:04:05 +0000

    build

--- dev.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run --rm -it -p 3000:3000 -v `pwd`:/app --workdir=/app node:alpine yarn start
--- npm.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run --rm -it -v `pwd`:/app --workdir=/app --entrypoint "npm" node:alpine
